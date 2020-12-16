a4090f447528245309aa9b5cfc5b951cc1f05da2
gopheramit <amitagrawal10@outlook.com>
2020-12-16 11:42:30 +0530

    fixed client id error

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"os"
 
 	"log"
 
@@ -15,32 +14,11 @@ import (
 	"github.com/markbates/goth/providers/google"
 )
 
-// func home(w http.ResponseWriter, r *http.Request) {
-
-// 	files := []string{
-// 		"./ui/html/home.page.tmpl",
-// 		"./ui/html/base.layout.tmpl",
-// 	}
-
-// 	ts, err := template.ParseFiles(files...)
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 		http.Error(w, "Internal Server Error", 500)
-// 		return
-// 	}
-// 	err = ts.Execute(w, nil)
-// 	if err != nil {
-// 		log.Println(err.Error())
-// 		http.Error(w, "Internal Server Error", 500)
-// 	}
-// 	//w.Write([]byte("HEllo from test"))
-// }
-
 func main() {
-	key := "DoN4QZCXaa3TJfr4BJZMQZNo" // Replace with your SESSION_SECRET or similar
+	key := "vlDxjmHJX80vOuHa5THxfCsR" // Replace with your SESSION_SECRET or similar
 	maxAge := 86400 * 30              // 30 days
 	isProd := false                   // Set to true when serving over https
-
+	clientid := "379756554270-olm9ma6g4dru3lil2cse84eaeimpj0u2.apps.googleusercontent.com"
 	store := sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
 	store.Options.Path = "/"
@@ -48,13 +26,9 @@ func main() {
 	store.Options.Secure = isProd
 
 	gothic.Store = store
-
-	// goth.UseProviders(
-	// 	google.New("263741611747-2bgmmh2vnbjvt02c3m8s30ujbb76obgf.apps.googleusercontent.com", "DoN4QZCXaa3TJfr4BJZMQZNo", "http://localhost:4000/auth/google/callback", "email", "profile"),
-	// )
 	goth.UseProviders(google.New(
-		os.Getenv("263741611747-2bgmmh2vnbjvt02c3m8s30ujbb76obgf.apps.googleusercontent.com"),
-		os.Getenv("DoN4QZCXaa3TJfr4BJZMQZNo"),
+		clientid,
+		key,
 		"http://localhost:4000/auth/callback?provider=google", "email", "profile"))
 
 	p := chi.NewRouter()
@@ -70,10 +44,6 @@ func main() {
 		t, _ := template.ParseFiles("templates/success.html")
 		t.Execute(res, user)
 	})
-
-	// p.Get("/auth", func(res http.ResponseWriter, req *http.Request) {
-	// 	gothic.BeginAuthHandler(res, req)
-	// })
 	p.Get("/auth", gothic.BeginAuthHandler)
 
 	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
@@ -83,10 +53,3 @@ func main() {
 	log.Println("listening on localhost:4000")
 	log.Fatal(http.ListenAndServe(":4000", p))
 }
-
-// mux := http.NewServeMux()
-// mux.HandleFunc("/", home)
-
-// log.Println("Starting server on :4000")
-// err := http.ListenAndServe(":4000", mux)
-// log.Fatal(err)
